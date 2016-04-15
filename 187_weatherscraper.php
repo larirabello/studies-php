461b6cf386cb32f541eb20e46f9b87050c9600d9
Larissa Rabello <lari.rabello@gmail.com>
2016-04-15 14:21:21 -0300

    Last project was made, but there is a problem in the regex

--- 187_weatherscraper.php
+++ 187_weatherscraper.php
@@ -18,7 +18,7 @@
     height: 100%;
     background-size: cover;
     background-position: center;
-    padding-top: 150px;
+    padding-top: 100px;
   }
 
   .center {
@@ -29,6 +29,12 @@
     color:white;
   }
 
+  .bcg {
+    background-color: rgba(255, 255, 255, 0.15);
+    padding: 30px;
+    border-radius: 10px;
+   }
+
   p {
     padding-top: 13px;
     padding-bottom: 13px;
@@ -36,7 +42,14 @@
 
   button {
     margin-top: 20px;
+    margin-bottom:20px;
   }
+
+  .alert {
+    margin-top:20px;
+    display:none;
+  }
+
 </style>
 
 </head>
@@ -45,15 +58,19 @@
 
     <div class="row">
 
-      <div class="col-md-6 col-md-offset-3 center">
+      <div class="col-md-6 col-md-offset-3 center bcg">
         <h1 class="center white">Weather Predictor</h1>
         <p class="lead center white">Enter your city below to get a forecast for the weather.</p>
         <form>
           <div class="form-group">
             <input type="text" class="form-control" name="city" id="city" placeholder="Eg. São Paulo, Paris, Milan..."/>
           </div>
-          <button class="btn btn-success btn-lg">Find My Weather</button>
+          <button id="findMyWeather" class="btn btn-success btn-lg">Find My Weather</button>
         </form>
+
+        <div id="success" class="alert alert-success">Success!</div>
+        <div id="fail" class="alert alert-danger">Could not find weather data for that city. Please try again.</div>
+        <div id="noCity" class="alert alert-danger"> Please  enter a city.</div>
       </div>
 
     </div>
@@ -64,5 +81,38 @@
   <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  <script>
+
+    $("#findMyWeather").click(function(event) {
+
+        event.preventDefault();
+
+        $(".alert").hide();
+
+        if ($("$city").val()!="") {
+
+          $.get("scraper.php?city="+$("#city").val(), function (data) {
+
+            if(data=="") {
+
+              $("#fail").html(data).fadeIn();
+
+            } else {
+
+              $("#success").html(data).fadeIn();
+
+            }
+
+          });
+
+        } else {
+
+          $("#noCity").fadeIn();
+
+        }
+
+    });
+
+  </script>
 </body>
 </html>
